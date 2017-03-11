4e474c722b104a3573440d82d24e36bfc5f1b292
Nicky Heise <nicky.heise@gmail.com>
2017-03-11 16:35:48 +0000

    add simon says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,31 @@
-#write your code here
+def echo(string)
+  string
+end
+
+def shout(string)
+  string.upcase
+end
+
+def repeat(string, times = 2)
+  ([string] * times).join(" ")
+end
+
+def start_of_word(string, number)
+  string[0...number]
+end
+
+def first_word(string)
+  string.split(" ").first
+end
+
+def titleize(string)
+  words = string.split.map do |word|
+    if %w(the and over).include?(word)
+      word
+    else
+      word.capitalize
+    end
+  end
+  words.first.capitalize!
+  words.join(" ")
+end
