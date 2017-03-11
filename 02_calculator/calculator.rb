9c0b814982cf15db1f220c53ae3e01689b20dbac
Nicky Heise <nicky.heise@gmail.com>
2017-03-11 14:57:08 +0000

    add complete calculator test

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,19 @@
-#write your code here
+def add(a, b)
+  a + b
+end
+
+def subtract(a, b)
+  a - b
+end
+
+def sum(numbers)
+  sum = 0
+  numbers.each { |number| sum += number}
+  sum
+end
+
+def multiply(*numbers)
+  product = 1
+  numbers.each { |number| product *= number }
+  product
+end
--- 02_calculator/calculator_spec.rb
+++ 02_calculator/calculator_spec.rb
@@ -79,10 +79,14 @@ end
 
 describe "#multiply" do
 
-  it "multiplies two numbers"
+  it "multiplies two numbers" do
+    expect(multiply(10,2)).to eq(20)
+  end
 
-  it "multiplies several numbers"
-  
+  it "multiplies several numbers" do
+    expect(multiply(10,2,3,7)).to eq(420)
+  end
+
 end
 
 describe "#power" do
